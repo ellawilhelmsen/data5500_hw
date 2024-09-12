965c6aaa132d388a5e9a89341f5673b5da2bf3f7
ellawilhelmsen <ellaw9981@gmail.com>
2024-09-11 18:19:58 -0600

    HW3 Test Push

---
+++ hw3/play_game.py
@@ -0,0 +1,32 @@
+'''
+This is an example of how to create a DeckOfCards object, shuffle it, and deal cards to play a game
+'''
+
+from DeckOfCards import *
+
+deck = DeckOfCards()
+deck.print_deck()
+deck.shuffle_deck()
+deck.print_deck()
+
+
+# deal two cards to the user
+card = deck.get_card()
+card2 = deck.get_card()
+
+score = 0
+# calculate the user's hand score
+score += card.val
+score += card2.val
+print("Your score is: ", score)
+
+
+# ask user if they would like a "hit" (another card)
+hit = input("would you like a hit? ")
+
+if hit == 'y':
+    card3 = deck.get_card()
+    score += card3.val
+    print("new score: ", score)
+    
+    
