b367459c006955ff1d7b27e3e8ce6ab688872c16
ellawilhelmsen <ellaw9981@gmail.com>
2024-09-11 23:39:46 -0600

    complete HW3 Blackjack

--- hw3/play_game.py
+++ hw3/play_game.py
@@ -4,29 +4,164 @@ This is an example of how to create a DeckOfCards object, shuffle it, and deal c
 
 from DeckOfCards import *
 
-deck = DeckOfCards()
-deck.print_deck()
-deck.shuffle_deck()
-deck.print_deck()
+
+# function to check for aces
+def check_ace(card_face, ace_count):
+    if card_face == "Ace":
+        ace_count += 1
+    return ace_count   
+
+play_again = 'y'
+
+while play_again == 'y':
+     
+    # create a deck of cards   
+    deck = DeckOfCards()
+    print()
+    print('Deck of Cards:')
+    deck.print_deck()
+    print()
+
+    # shuffle the deck
+    deck.shuffle_deck()
+    print('Shuffled Deck of Cards:')
+    deck.print_deck()
+    print()
 
 
-# deal two cards to the user
-card = deck.get_card()
-card2 = deck.get_card()
-
-score = 0
-# calculate the user's hand score
-score += card.val
-score += card2.val
-print("Your score is: ", score)
+    # set up for ace value adjustment
+    aces = 0
+    dealer_aces = 0
 
 
-# ask user if they would like a "hit" (another card)
-hit = input("would you like a hit? ")
+    # deal two cards to the user and check for aces
+    card = deck.get_card()
+    aces = check_ace(card.face, aces)
 
-if hit == 'y':
-    card3 = deck.get_card()
-    score += card3.val
-    print("new score: ", score)
-    
-    
+    card2 = deck.get_card()
+    aces = check_ace(card2.face, aces)
+
+
+    # deal two cards to the dealer and check for aces
+    dealer_card = deck.get_card()
+    dealer_aces = check_ace(dealer_card.face, dealer_aces)
+
+    dealer_card2 = deck.get_card()
+    dealer_aces = check_ace(dealer_card2.face, dealer_aces)
+
+    # set up scores for user and dealer
+    score = 0
+    dealer_score = 0
+
+    # calculate the user's and dealer's hand score
+    score += card.val
+    score += card2.val
+
+    dealer_score += dealer_card.val
+    dealer_score += dealer_card2.val
+
+    # print users cards and score
+    print()
+    print(f'Card one: {card}')
+    print(f'Card two: {card2}')
+    print()
+    print("Your score is: ", score)
+
+    # set up for user to take a hit
+    cont = 0
+
+    # blackjack check
+    if score == 21:
+        print("Blackjack! You win!")
+        play_again = input("Would you like to play again? (y/n)").lower()
+
+    else:
+        while cont == 0:
+            # ask user if the want to take a hit
+            hit = input("Would you like a hit? (y/n)").lower()
+
+            if hit == 'y':
+                # deal another card to the user, check for aces, calculate score
+                card3 = deck.get_card()
+                aces = check_ace(card3.face, aces)
+                score += card3.val
+                print(f'New Card: {card3}')
+
+                if score > 21 and aces > 0:
+                    # adjust score for aces
+                    score -= 10
+                    aces -= 1
+
+                elif score > 21:
+                    # check for a bust
+                    print("You busted. You lose.")
+                    print()
+                    # ask user if they want to play again
+                    play_again = input("Would you like to play again? (y/n)").lower()
+                    cont = 1
+
+                # print new score
+                print("New Score: ", score)
+                
+            elif hit == 'n':
+                # end users turn
+                print("Your final score is: ", score)  
+                cont = 1
+
+                # calculate the dealer's hand score
+                print()
+                print("Dealer's Turn:")
+                print(f'Dealer card one: {dealer_card}')
+                print(f'Dealer card two: {dealer_card2}')
+                print()
+
+                if dealer_score == 21:
+                    # check for dealer blackjack
+                    print("Dealer has blackjack, and wins.")
+                    print()
+                    # ask user if they want to play again
+                    play_again = input("Would you like to play again? (y/n)").lower()
+                    break
+                else:
+                    while dealer_score < 17:
+                        # dealer must hit on 16 or less
+                        # deal another card to the dealer, check for aces, calculate score
+                        dealer_card3 = deck.get_card()
+                        print(f'Dealer hits, new card: {dealer_card3}')
+                        dealer_aces = check_ace(dealer_card3.face, dealer_aces)
+                        dealer_score += dealer_card3.val
+
+                        if dealer_score > 21 and dealer_aces > 0:
+                            # adjust score for aces
+                            dealer_score -= 10
+                            dealer_aces -= 1
+
+                        elif dealer_score > 21:
+                            # check for dealer bust
+                            print("You win! Dealer busted. Yay!")
+                            print()
+
+                            # ask user if they want to play again
+                            play_again = input("Would you like to play again? (y/n)").lower()
+                            break
+                    else:   
+                        print("Dealer Score: ", dealer_score)
+                        print()
+
+                        # check for winner, deliver results
+                        if dealer_score > score:
+                            print("Dealer wins. Their score is higher than yours.")
+                        elif dealer_score == score:
+                            print("It's a push. You and the dealer have the same score.")
+                        else:
+                            print("You win! Your score is higher than the dealer's.")
+                        print()
+
+                        # ask user if they want to play again
+                        play_again = input("Would you like to play again? (y/n)").lower()    
+
+            else: 
+                print('please enter y or n')     
+
+
+        
